fb634e67550a5eb1d3a66fbe4e0c47842101ed43
ButWhatIfNotPony <jamieshorey42@gmail.com>
2022-11-13 18:27:29 +0000

    Added to Converter Cog

--- cogs/converter.py
+++ cogs/converter.py
@@ -11,7 +11,7 @@ class Converters(commands.Cog):
     @commands.command(name='miles2km', aliases=['m2km', 'miles2kilometers'])
     async def milesToKilometers(self, ctx, dist : float):
         """Converts Miles into Kilometers for us Metric CHADS."""
-        total = dist * 1.609
+        total = round(dist * 1.609, 3)
         await ctx.send(f'{dist} Miles is approximately {total}KM.')
 
 
@@ -20,6 +20,19 @@ class Converters(commands.Cog):
         """Converts Kilometers into Miles for you Imperial PEASANTS."""
         total = round((dist / 1.609), 3)
         await ctx.send(f'{dist}KM is approximately {total} Miles.')
+
+    
+    @commands.command(name='cels2fahr', aliases=['c2f', 'celsius2fahrenheit'])
+    async def celciusToFahrenheit(self, ctx, temp : float):
+        """Converts Celsius to Fahrenheit, for whatever reason you would prefer that."""
+        total = round((temp * 1.8) + 32, 3)
+        await ctx.send(f'{temp}' + chr(176) + 'C is approximatley {total}' + chr(176) + 'F.')
+
+    @commands.command(name='fahr2cels', aliases=['f2c', 'fahrenheit2celsius'])
+    async def fahrenheitToCelsius(self, ctx, temp : float):
+        """Converts Fahrenheit to Celsius, for the better unit of temperature."""
+        total = round((temp -32) * 0.5556, 3)
+        await ctx.send(f'{temp}' + chr(176) + 'F is approximately {total}' + chr(176) + 'C.')
         
     
 # The setup function below is neccersary. Remeber we give bot.add_cog() the name of the class in this case MembersCog.
